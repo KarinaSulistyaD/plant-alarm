fd400d5b0e2b31f1ba4454cfae6ba356844a8ada
sembak1 <faizacer12345@gmail.com>
2024-10-31 10:28:14 +0700

    git push

--- lib/main.dart
+++ lib/main.dart
@@ -50,7 +50,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'tasbeh online:',
+              'pembawa pahala online:',
             ),
             Text(
               '$_counter',
